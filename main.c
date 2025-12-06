0c02adb11cf411e747e4c896105e520ebf5a3605
Isaac <isaacshagal329@gmail.com>
2025-12-06 16:53:26 +0800

    Updated main.c with new code

--- main.c
+++ main.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(int argc, char*argv[]){
+        int a = 13;
+        int b = 10;
+        print("Simple Calculator\n");
+        print("a+b = %d\n",add_function(a,b));
+        print("a-b = %d\n",add_function(a,b));
+        print("a+b = %d\n",add_function(a,b));
+}
